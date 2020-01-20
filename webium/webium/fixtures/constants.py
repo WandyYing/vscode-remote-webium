631508e5976153c6fee19c0dd446f52b4c4e0c71
jun <wandy1208@gmail.com>
2020-01-20 20:19:40 +0800

    add jquery 3.4.1.min.js

--- webium/webium/fixtures/constants.py
+++ webium/webium/fixtures/constants.py
@@ -6,3 +6,4 @@ Webium constants are stored in this file.
 class JQuery:
     VER = "3.4.1"
     MIN_JS = "//cdnjs.cloudflare.com/ajax/libs/jquery/%s/jquery.min.js" % VER
+    LOCAL_JQUERY_PATH = './webium/jquery/jquery-3.4.1.min.js' #todo: Local
--- webium/webium/jquery/__init__.py
+++ webium/webium/jquery/__init__.py
@@ -47,21 +47,15 @@ class JQuery(object):
       e = driver.find_element_by_id('id_name')
       JQuery(e).val('New name')
     """
-    JQUERY_PATH = './webium/jquery/jquery-1.10.2.js'
 
     def __init__(self, element):
         self.driver = get_driver()
-        # if not self.driver.execute_script('return window.jQuery'):
-        #     with open(self.JQUERY_PATH, 'r') as jquery:
-        #         self.driver.execute_script(jquery.read())
-        #         print("jquery read")
         activate_jquery(self.driver)
         self.element = element
 
     def __getattr__(self, name):
         def jquery_func(*args):
             jquery = 'return $(arguments[0]).%(func)s(%(args)s);' % {
-            # jquery = 'return arguments[0].%(func)s(%(args)s);' % {
                 'func': name,
                 'args': ','.join(['arguments[%d]' % (1 + i) for i in range(len(args))])
             }
